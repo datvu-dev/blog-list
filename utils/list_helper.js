54ed96aef95c7dc16709ad132ff9ddfb5259c5f0
Dat Vu <dothanhdat.vu@gmail.com>
2019-11-24 10:00:09 +1100

    Write unit test for api

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,21 @@ const totalLikes = (blogs) => {
     return blogs.length ? likesArray.reduce(reducer) : 0
 }
 
+const favoriteBlog = (blogs) => {
+    const likesArray = blogs.map(blog => blog.likes)
+    const i = likesArray.indexOf(Math.max(...likesArray))
+
+    blogObj = {
+        title: blogs[i].title,
+        author: blogs[i].author,
+        likes: blogs[i].likes
+    }
+
+    return blogObj
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
