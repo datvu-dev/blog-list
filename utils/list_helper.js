ca94fe853a2ac70cf788447d31ce0cd8107944dc
Dat Vu <dothanhdat.vu@gmail.com>
2019-11-23 21:31:11 +1100

    Added a test function to return total likes of a blog list

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likesArray = blogs.map(blog => blog.likes)
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+    return blogs.length ? likesArray.reduce(reducer) : 0
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
