5b39f88271ef26002f70b908524a4c2aae89d413
Dat Vu <dothanhdat.vu@gmail.com>
2019-11-30 09:38:09 +1100

    Make posting blogs only possible when the request has a valid token attached

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -22,23 +23,45 @@ blogsRouter.get('/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+
+    return null
+}
+
 blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const user = await User.findById(body.userId)
-
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-        user: user._id
-    })
-
+    const token = getTokenFrom(request)
     try {
+        const decodedToken = jwt.verify(token, process.env.SECRET)
+
+        if (!token || !decodedToken.id) {
+            return response.status(401).json({ error: 'token missing or invalid' })
+        }
+
+        const user = await User.findById(body.userId)
+
+        const blog = new Blog({
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes,
+            user: user._id
+        })
+
+
         const savedBlog = await blog.save()
         user.blogs = user.blogs.concat(savedBlog._id)
         await user.save()
+
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+
         response.json(savedBlog.toJSON())
     } catch(exception) {
         next(exception)
